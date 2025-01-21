21b37e8d0440885255ede518441d2786df200cff
knightingal <yujianming1985@hotmail.com>
2025-01-21 21:43:06 +0800

    create `MilitaryHTTPRequestHandler` class

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
+
+
 def print_hello():
   print("Hello")
 
 
+class MilitaryHTTPRequestHandler(SimpleHTTPRequestHandler):
+  pass
+
+
+def run(server_class=HTTPServer, handler_class=MilitaryHTTPRequestHandler):
+  server_address=('', 8000)
+  httpd = server_class(server_address, handler_class)
+  httpd.serve_forever()
+
+
 if __name__ == "__main__":
   print_hello()
+  run()
